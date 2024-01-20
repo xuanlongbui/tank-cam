a4a8a8f6708d613751fe45ba4d3c8c83078eee24
xuanlongbui <xuanlong1372000@gmail.com>
2024-01-20 14:41:40 +0700

    add basic function

---
+++ arduinoR3src/arduinoR3src/arduinoR3src.ino
@@ -0,0 +1,130 @@
+#include <Arduino_JSON.h>
+
+#define STOP 0
+#define FORWARD 1
+#define BACKWARD 2
+#define RIGHT 3
+#define LEFT 4
+int direct = 0;
+//motor A
+const int APin1 = 2;
+const int PWMA = 3;
+const int APin2 = 4;
+int speed1 = 0;
+//Motor B
+const int BPin1 = 5;
+const int PWMB = 6;
+const int BPin2 = 7;
+int speed2 = 0;
+//This will run only one time.
+String content = "";
+char character;
+
+void forward() {
+  digitalWrite(APin1, 0);
+  digitalWrite(APin2, 1);
+  digitalWrite(BPin1, 0);
+  digitalWrite(BPin2, 1);
+}
+void backward() {
+  digitalWrite(APin1, 1);
+    digitalWrite(APin2, 0);
+  digitalWrite(BPin1, 1);
+  digitalWrite(BPin2, 0);
+}
+void left() {
+  digitalWrite(APin1, 1);
+    digitalWrite(APin2, 0);
+  digitalWrite(BPin1, 0);
+  digitalWrite(BPin2, 1);
+}
+void right() {
+  digitalWrite(APin1, 0);
+    digitalWrite(APin2, 1);
+  digitalWrite(BPin1, 1);
+  digitalWrite(BPin2, 0);
+}
+void stop() {
+  digitalWrite(APin1, 0);
+    digitalWrite(APin2, 0);
+  digitalWrite(BPin1, 0);
+  digitalWrite(BPin2, 0);
+}
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  //Set pins as outputs
+  pinMode(APin1, OUTPUT);
+  pinMode(PWMA, OUTPUT);
+  pinMode(APin2, OUTPUT);
+  pinMode(BPin1, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+  pinMode(BPin2, OUTPUT);
+}
+void motorHandle() {
+  switch (direct) {
+    case STOP:
+      stop();
+      break;
+    case FORWARD:
+      forward();
+       break;
+    case BACKWARD:
+      backward();
+       break;
+    case RIGHT:
+      right();
+      break;
+    case LEFT:
+      left();
+      break;
+    default:
+      Serial.println("invalid direct");
+  }
+
+analogWrite(PWMA,speed1);
+analogWrite(PWMB,speed2);
+}
+void dataHandle(String input) {
+  JSONVar myObject = JSON.parse(input);
+
+  // JSON.typeof(jsonVar) can be used to get the type of the variable
+  if (JSON.typeof(myObject) == "undefined") {
+    Serial.println("Parsing input failed!");
+    return;
+  }
+
+  Serial.print("JSON.typeof(myObject) = ");
+  Serial.println(JSON.typeof(myObject));  // prints: object
+
+  if (myObject.hasOwnProperty("direct")) {
+    Serial.print("myObject[\"direct\"] = ");
+    Serial.println((int)myObject["direct"]);
+    direct = (int)myObject["direct"];
+  }
+  if (myObject.hasOwnProperty("speed1")) {
+    Serial.print("myObject[\"speed1\"] = ");
+    Serial.println((int)myObject["speed1"]);
+    speed1 = (int)myObject["speed1"];
+  }
+  if (myObject.hasOwnProperty("speed2")) {
+    Serial.print("myObject[\"speed2\"] = ");
+    Serial.println((int)myObject["speed2"]);
+    speed2 = (int)myObject["speed2"];
+  }
+
+}
+
+void loop() {
+  while (Serial.available()) {
+    character = Serial.read();
+    content.concat(character);
+  }
+    if (content != "" && character == '\n') {
+      Serial.println(content);
+      dataHandle(content);
+      Serial.flush();
+      content = "";
+      motorHandle();
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,402 @@
 #include "Arduino.h"
+#include <ESPmDNS.h>
+#include <SPIFFS.h>
+#include <WiFi.h>
 
-void setup() {
 
-  Serial.begin(96a);
-  Serial.setDebugOutput(true);
-  Serial.println();
-  Serial.println("{\"direct\":1, \"speed1\":100, \"speed2\":100}");
+#include "esp_camera.h"
 
+#include "esp_timer.h"
+#include "img_converters.h"
+
+#include "fb_gfx.h"
+#include "soc/soc.h"             // disable brownout problems
+#include "soc/rtc_cntl_reg.h"    // disable brownout problems
+#include "esp_http_server.h"
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+// direct control
+#define STOP 0
+#define FORWARD 1
+#define BACKWARD 2
+#define RIGHT 3
+#define LEFT 4
+//AI Thinker
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
+#define PART_BOUNDARY "123456789000000000000987654321"
+
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+httpd_handle_t camera_httpd = NULL;
+httpd_handle_t stream_httpd = NULL;
+static const char PROGMEM INDEX_HTML[] = R"rawliteral(
+<html>
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <style>
+    * {
+      touch-action: manipulation;
+    }
+
+    *{
+      -webkit-touch-callout: none;
+      /* iOS Safari */
+      -webkit-user-select: none;
+      /* Safari */
+      -khtml-user-select: none;
+      /* Konqueror HTML */
+      -moz-user-select: none;
+      /* Old versions of Firefox */
+      -ms-user-select: none;
+      /* Internet Explorer/Edge */
+      user-select: none;
+      /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
+    }
+
+    body {
+      font-family: Arial;
+      text-align: center;
+      margin: 0px auto;
+      padding-top: 30px;
+    }
+
+    table {
+      margin-left: auto;
+      margin-right: auto;
+    }
+
+    td {
+      padding: 8 px;
+    }
+
+    .button {
+      background-color: #2f4468;
+      border: none;
+      color: white;
+      padding: 10px 20px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 18px;
+      margin: 6px 3px;
+      cursor: pointer;
+      -webkit-touch-callout: none;
+      -webkit-user-select: none;
+      -khtml-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+      -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    }
+
+    .slider {
+      -webkit-appearance: none;
+      margin: 14px;
+      width: 360px;
+      height: 25px;
+      background: #FFD65C;
+      outline: none;
+      -webkit-transition: .2s;
+      transition: opacity .2s;
+    }
+
+    .slider::-webkit-slider-thumb {
+      -webkit-appearance: none;
+      appearance: none;
+      width: 35px;
+      height: 35px;
+      background: #003249;
+      cursor: pointer;
+    }
+
+    .slider::-moz-range-thumb {
+      width: 35px;
+      height: 35px;
+      background: #003249;
+      cursor: pointer;
+    }
+    img {  width: auto ;
+        max-width: 100% ;
+        height: auto ; 
+      }
+  </style>
+</head>
+
+<body>
+  <script>
+    let obj = {
+      'direct': 'stop',
+      'speed1': 50,
+      'speed2': 50
+    }
+    async function driectControl(param) {
+      obj.direct = param;
+      console.log(obj);
+
+      try {
+          const response = await fetch("http://tankServer.local/control", {
+            method: "POST",
+            body: JSON.stringify(obj),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+        } catch (error) {
+          alert("Request failed - check the console");
+          console.error(error);
+        }
+    }
+    function speedControl1() {
+      var sliderValue = document.getElementById("pwmSlider1").value;
+      obj.speed1 = sliderValue;
+      document.getElementById("textSliderValue1").innerHTML = "Motor1: "+ sliderValue;
+    }
+    function speedControl2() {
+      var sliderValue = document.getElementById("pwmSlider2").value;
+      obj.speed2 = sliderValue;
+      document.getElementById("textSliderValue2").innerHTML = "Motor2: "+ sliderValue;
+    }
+    window.onload = document.getElementById("photo").src = window.location.href.slice(0, -1) + ":81/stream";
+  </script>
+  <img src="" id="photo" >
+  <p><span id="textSliderValue1" class="noselect"> Motor1: 50</span></p>
+  <p><input type="range" onchange="speedControl1()" id="pwmSlider1" min="0" max="255" value="50" step="1"
+      class="slider"></p>
+  <p><span id="textSliderValue2" class="noselect"> Motor2: 50</span></p>
+  <p><input type="range" onchange="speedControl2()" id="pwmSlider2" min="0" max="255" value="50" step="1"
+      class="slider"></p>
+  <table>
+    <tr>
+      <td colspan="3" align="center"><button class="button" onmousedown="driectControl('forward');"
+          ontouchstart="driectControl('forward');" onmouseup="driectControl('stop');"
+          ontouchend="driectControl('stop');">Forward</button></td>
+    </tr>
+    <tr>
+      <td align="center"><button class="button" ontouchstart="driectControl('left');"
+          ontouchend="driectControl('stop');">Left</button></td>
+      <td align="center"><button class="button" onmousedown="driectControl('stop');"
+          ontouchstart="driectControl('stop');">Stop</button></td>
+      <td align="center"><button class="button" ontouchstart="driectControl('right');"
+          ontouchend="driectControl('stop');">Right</button></td>
+    </tr>
+    <tr>
+      <td colspan="3" align="center"><button class="button" ontouchstart="driectControl('backward');"
+          ontouchend="driectControl('stop');">Backward</button></td>
+    </tr>
+  </table>
+</body>
+</html>
+)rawliteral";
+
+static esp_err_t index_handler(httpd_req_t *req){
+  httpd_resp_set_type(req, "text/html");
+  return httpd_resp_send(req, (const char *)INDEX_HTML, strlen(INDEX_HTML));
+}
+static esp_err_t stream_handler(httpd_req_t *req){
+  camera_fb_t * fb = NULL;
+  esp_err_t res = ESP_OK;
+  size_t _jpg_buf_len = 0;
+  uint8_t * _jpg_buf = NULL;
+  char * part_buf[64];
+
+  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+  if(res != ESP_OK){
+    return res;
+  }
+
+  while(true){
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Camera capture failed");
+      res = ESP_FAIL;
+    } else {
+      if(fb->width > 400){
+        if(fb->format != PIXFORMAT_JPEG){
+          bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+          esp_camera_fb_return(fb);
+          fb = NULL;
+          if(!jpeg_converted){
+            Serial.println("JPEG compression failed");
+            res = ESP_FAIL;
+          }
+        } else {
+          _jpg_buf_len = fb->len;
+          _jpg_buf = fb->buf;
+        }
+      }
+    }
+    if(res == ESP_OK){
+      size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+    }
+    if(res == ESP_OK){
+      res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+    }
+    if(res == ESP_OK){
+      res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+    }
+    if(fb){
+      esp_camera_fb_return(fb);
+      fb = NULL;
+      _jpg_buf = NULL;
+    } else if(_jpg_buf){
+      free(_jpg_buf);
+      _jpg_buf = NULL;
+    }
+    if(res != ESP_OK){
+      break;
+    }
+    //Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
+  }
+  return res;
 }
 
-void loop() {
+static esp_err_t cmd_handler(httpd_req_t *req){
+    /* Destination buffer for content of HTTP POST request.
+     * httpd_req_recv() accepts char* only, but content could
+     * as well be any binary data (needs type casting).
+     * In case of string data, null termination will be absent, and
+     * content length would give length of string */
+    char content[100];
+
+    /* Truncate if content length larger than the buffer */
+    size_t recv_size = MIN(req->content_len, sizeof(content));
+
+    int ret = httpd_req_recv(req, content, recv_size);
+    if (ret <= 0) {  /* 0 return value indicates connection closed */
+        /* Check if timeout occurred */
+        if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
+            /* In case of timeout one can choose to retry calling
+             * httpd_req_recv(), but to keep it simple, here we
+             * respond with an HTTP 408 (Request Timeout) error */
+            httpd_resp_send_408(req);
+        }
+        /* In case of error, returning ESP_FAIL will
+         * ensure that the underlying socket is closed */
+        return ESP_FAIL;
+    }else{
+      Serial.println(content);
+    }
+
+    /* Send a simple response */
+    const char resp[] = "URI POST Response";
+    httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+void startCameraServer(){
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+  config.server_port = 80;
+  httpd_uri_t index_uri = {
+    .uri       = "/",
+    .method    = HTTP_GET,
+    .handler   = index_handler,
+    .user_ctx  = NULL
+  };
+
+  httpd_uri_t cmd_uri = {
+    .uri       = "/control",
+    .method    = HTTP_POST,
+    .handler   = cmd_handler,
+    .user_ctx  = NULL
+  };
+  httpd_uri_t stream_uri = {
+    .uri       = "/stream",
+    .method    = HTTP_GET,
+    .handler   = stream_handler,
+    .user_ctx  = NULL
+  };
+  if (httpd_start(&camera_httpd, &config) == ESP_OK) {
+    httpd_register_uri_handler(camera_httpd, &index_uri);
+    httpd_register_uri_handler(camera_httpd, &cmd_uri);
+  }
+  config.server_port += 1;
+  config.ctrl_port += 1;
+  if (httpd_start(&stream_httpd, &config) == ESP_OK) {
+    httpd_register_uri_handler(stream_httpd, &stream_uri);
+  }
+}
+
+void setup()
+{
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+  Serial.begin(9600);
+  Serial.setDebugOutput(true);
+  SPIFFS.begin();
+    camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG; 
+  
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_VGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+    
+  // Camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+  Serial.println("Setting AP (Access Point)");
+  // NULL sets an open Access Point
+  WiFi.softAP("ESP-WIFI", NULL);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+  // MDNS.begin("tankServer");
+    // Start streaming web server
+  startCameraServer();
+}
+
+void loop()
+{
   // Do nothing. Everything is done in another task by the web server
-  delay(1000);
-  Serial.println("{\"direct\":1, \"speed1\":100, \"speed2\":100}");
-  delay(1000);
-  Serial.println("{\"direct\":0, \"speed1\":200, \"speed2\":200}");
+  // delay(1000);
+  // Serial.println("{\"direct\":1, \"speed1\":100, \"speed2\":100}");
+  // delay(1000);
+  // Serial.println("{\"direct\":0, \"speed1\":200, \"speed2\":200}");
 }
