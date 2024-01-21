fe0e1ecc1f53cec7c09918d9dd70bce6dfdb7af9
xuanlongbui <xuanlong1372000@gmail.com>
2024-01-21 23:30:37 +0700

    stream

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,26 @@
 #include "Arduino.h"
+#include <ArduinoJson.h>
 #include <ESPmDNS.h>
 #include <SPIFFS.h>
 #include <WiFi.h>
-
-
 #include "esp_camera.h"
-
+#include <ArduinoJson.h>
 #include "esp_timer.h"
 #include "img_converters.h"
 
 #include "fb_gfx.h"
-#include "soc/soc.h"             // disable brownout problems
-#include "soc/rtc_cntl_reg.h"    // disable brownout problems
+#include "soc/soc.h"          // disable brownout problems
+#include "soc/rtc_cntl_reg.h" // disable brownout problems
 #include "esp_http_server.h"
 
-#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
 // direct control
 #define STOP 0
 #define FORWARD 1
 #define BACKWARD 2
 #define RIGHT 3
 #define LEFT 4
-//AI Thinker
+// AI Thinker
 #define PWDN_GPIO_NUM 32
 #define RESET_GPIO_NUM -1
 #define XCLK_GPIO_NUM 0
@@ -41,12 +40,16 @@
 #define PCLK_GPIO_NUM 22
 #define PART_BOUNDARY "123456789000000000000987654321"
 
-static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
-static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
-static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+static const char *_STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char *_STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char *_STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
 
 httpd_handle_t camera_httpd = NULL;
 httpd_handle_t stream_httpd = NULL;
+
+StaticJsonDocument<200> doc;
+StaticJsonDocument<200> docOut;
+
 static const char PROGMEM INDEX_HTML[] = R"rawliteral(
 <html>
 
@@ -176,7 +179,6 @@ static const char PROGMEM INDEX_HTML[] = R"rawliteral(
       obj.speed2 = sliderValue;
       document.getElementById("textSliderValue2").innerHTML = "Motor2: "+ sliderValue;
     }
-    window.onload = document.getElementById("photo").src = window.location.href.slice(0, -1) + ":81/stream";
   </script>
   <img src="" id="photo" >
   <p><span id="textSliderValue1" class="noselect"> Motor1: 50</span></p>
@@ -204,145 +206,231 @@ static const char PROGMEM INDEX_HTML[] = R"rawliteral(
           ontouchend="driectControl('stop');">Backward</button></td>
     </tr>
   </table>
+  <script>
+  window.onload = document.getElementById("photo").src = "http://tankServer.local:81/stream";
+  </script>
 </body>
 </html>
 )rawliteral";
 
-static esp_err_t index_handler(httpd_req_t *req){
+static esp_err_t index_handler(httpd_req_t *req)
+{
   httpd_resp_set_type(req, "text/html");
   return httpd_resp_send(req, (const char *)INDEX_HTML, strlen(INDEX_HTML));
 }
-static esp_err_t stream_handler(httpd_req_t *req){
-  camera_fb_t * fb = NULL;
+static esp_err_t stream_handler(httpd_req_t *req)
+{
+  camera_fb_t *fb = NULL;
   esp_err_t res = ESP_OK;
   size_t _jpg_buf_len = 0;
-  uint8_t * _jpg_buf = NULL;
-  char * part_buf[64];
+  uint8_t *_jpg_buf = NULL;
+  char *part_buf[64];
 
   res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
-  if(res != ESP_OK){
+  if (res != ESP_OK)
+  {
     return res;
   }
 
-  while(true){
+  while (true)
+  {
     fb = esp_camera_fb_get();
-    if (!fb) {
+    if (!fb)
+    {
       Serial.println("Camera capture failed");
       res = ESP_FAIL;
-    } else {
-      if(fb->width > 400){
-        if(fb->format != PIXFORMAT_JPEG){
+    }
+    else
+    {
+      if (fb->width > 400)
+      {
+        if (fb->format != PIXFORMAT_JPEG)
+        {
           bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
           esp_camera_fb_return(fb);
           fb = NULL;
-          if(!jpeg_converted){
+          if (!jpeg_converted)
+          {
             Serial.println("JPEG compression failed");
             res = ESP_FAIL;
           }
-        } else {
+        }
+        else
+        {
           _jpg_buf_len = fb->len;
           _jpg_buf = fb->buf;
         }
       }
     }
-    if(res == ESP_OK){
+    if (res == ESP_OK)
+    {
       size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
       res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
     }
-    if(res == ESP_OK){
+    if (res == ESP_OK)
+    {
       res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
     }
-    if(res == ESP_OK){
+    if (res == ESP_OK)
+    {
       res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
     }
-    if(fb){
+    if (fb)
+    {
       esp_camera_fb_return(fb);
       fb = NULL;
       _jpg_buf = NULL;
-    } else if(_jpg_buf){
+    }
+    else if (_jpg_buf)
+    {
       free(_jpg_buf);
       _jpg_buf = NULL;
     }
-    if(res != ESP_OK){
+    if (res != ESP_OK)
+    {
       break;
     }
-    //Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
+    // Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
   }
   return res;
 }
-
-static esp_err_t cmd_handler(httpd_req_t *req){
-    /* Destination buffer for content of HTTP POST request.
-     * httpd_req_recv() accepts char* only, but content could
-     * as well be any binary data (needs type casting).
-     * In case of string data, null termination will be absent, and
-     * content length would give length of string */
-    char content[100];
-
-    /* Truncate if content length larger than the buffer */
-    size_t recv_size = MIN(req->content_len, sizeof(content));
-
-    int ret = httpd_req_recv(req, content, recv_size);
-    if (ret <= 0) {  /* 0 return value indicates connection closed */
-        /* Check if timeout occurred */
-        if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
-            /* In case of timeout one can choose to retry calling
-             * httpd_req_recv(), but to keep it simple, here we
-             * respond with an HTTP 408 (Request Timeout) error */
-            httpd_resp_send_408(req);
-        }
-        /* In case of error, returning ESP_FAIL will
-         * ensure that the underlying socket is closed */
-        return ESP_FAIL;
-    }else{
-      Serial.println(content);
+void uart_cmd(String msg_input){
+  int direction = 0;
+    // Serial.println(msg_input);
+    DeserializationError error = deserializeJson(doc, msg_input);
+    // Test if parsing succeeds.
+    if (error)
+    {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
     }
+    const char* direct = doc["direct"];
+    // Serial.println(direct);
+    
+    if (strcmp(doc["direct"],"stop") == 0)
+    {
+      direction = STOP;
+    }
+    else if (strcmp(doc["direct"],"forward") == 0)
+    {
+      direction = FORWARD;
+    }
+    else if (strcmp(doc["direct"],"backward") == 0)
+    {
+      direction = BACKWARD;
+    }
+    else if (strcmp(doc["direct"],"right") == 0)
+    {
+      direction = RIGHT;
+    }
+    else if (strcmp(doc["direct"],"left") == 0)
+    {
+      direction = LEFT;
+    }
+    else
+    {
+      //
+    }
+    docOut["direct"] = direction;
+    docOut["speed1"] = doc["speed1"];
+    docOut["speed2"] = doc["speed2"];
 
-    /* Send a simple response */
-    const char resp[] = "URI POST Response";
-    httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
-    return ESP_OK;
+    serializeJson(docOut, Serial);
+    Serial.println();
 }
-void startCameraServer(){
+static esp_err_t cmd_handler(httpd_req_t *req)
+{
+  /* Destination buffer for content of HTTP POST request.
+   * httpd_req_recv() accepts char* only, but content could
+   * as well be any binary data (needs type casting).
+   * In case of string data, null termination will be absent, and
+   * content length would give length of string */
+  char content[100];
+  int ind_ct = 0;
+  char character;
+  String output_str = "";
+  /* Truncate if content length larger than the buffer */
+  size_t recv_size = MIN(req->content_len, sizeof(content));
+
+  int ret = httpd_req_recv(req, content, recv_size);
+  if (ret <= 0)
+  { /* 0 return value indicates connection closed */
+    /* Check if timeout occurred */
+    if (ret == HTTPD_SOCK_ERR_TIMEOUT)
+    {
+      /* In case of timeout one can choose to retry calling
+       * httpd_req_recv(), but to keep it simple, here we
+       * respond with an HTTP 408 (Request Timeout) error */
+      httpd_resp_send_408(req);
+    }
+    /* In case of error, returning ESP_FAIL will
+     * ensure that the underlying socket is closed */
+    return ESP_FAIL;
+  }
+  else
+  {
+    for (int i = 0; i < 100; i++)
+    {
+      character = content[i];
+      if (character != '}')
+      {
+        output_str.concat(character);
+      }
+      else
+      {
+        output_str.concat(character);
+        break;
+      }
+    }
+  uart_cmd(output_str);
+  }
+
+  /* Send a simple response */
+  const char resp[] = "URI POST Response";
+  httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+  return ESP_OK;
+}
+
+void startCameraServer()
+{
   httpd_config_t config = HTTPD_DEFAULT_CONFIG();
   config.server_port = 80;
   httpd_uri_t index_uri = {
-    .uri       = "/",
-    .method    = HTTP_GET,
-    .handler   = index_handler,
-    .user_ctx  = NULL
-  };
+      .uri = "/",
+      .method = HTTP_GET,
+      .handler = index_handler,
+      .user_ctx = NULL};
 
   httpd_uri_t cmd_uri = {
-    .uri       = "/control",
-    .method    = HTTP_POST,
-    .handler   = cmd_handler,
-    .user_ctx  = NULL
-  };
+      .uri = "/control",
+      .method = HTTP_POST,
+      .handler = cmd_handler,
+      .user_ctx = NULL};
   httpd_uri_t stream_uri = {
-    .uri       = "/stream",
-    .method    = HTTP_GET,
-    .handler   = stream_handler,
-    .user_ctx  = NULL
-  };
-  if (httpd_start(&camera_httpd, &config) == ESP_OK) {
+      .uri = "/stream",
+      .method = HTTP_GET,
+      .handler = stream_handler,
+      .user_ctx = NULL};
+  if (httpd_start(&camera_httpd, &config) == ESP_OK)
+  {
     httpd_register_uri_handler(camera_httpd, &index_uri);
     httpd_register_uri_handler(camera_httpd, &cmd_uri);
   }
   config.server_port += 1;
   config.ctrl_port += 1;
-  if (httpd_start(&stream_httpd, &config) == ESP_OK) {
+  if (httpd_start(&stream_httpd, &config) == ESP_OK)
+  {
     httpd_register_uri_handler(stream_httpd, &stream_uri);
   }
 }
 
 void setup()
 {
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // disable brownout detector
   Serial.begin(9600);
   Serial.setDebugOutput(true);
   SPIFFS.begin();
-    camera_config_t config;
+  camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
   config.pin_d0 = Y2_GPIO_NUM;
@@ -362,33 +450,37 @@ void setup()
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG; 
-  
-  if(psramFound()){
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  if (psramFound())
+  {
     config.frame_size = FRAMESIZE_VGA;
     config.jpeg_quality = 10;
     config.fb_count = 2;
-  } else {
+  }
+  else
+  {
     config.frame_size = FRAMESIZE_SVGA;
     config.jpeg_quality = 12;
     config.fb_count = 1;
   }
-    
+
   // Camera init
   esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
+  if (err != ESP_OK)
+  {
     Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
-  Serial.println("Setting AP (Access Point)");
+  // Serial.println("Setting AP (Access Point)");
   // NULL sets an open Access Point
   WiFi.softAP("ESP-WIFI", NULL);
 
   IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(IP);
-  // MDNS.begin("tankServer");
-    // Start streaming web server
+  // Serial.print("AP IP address: ");
+  // Serial.println(IP);
+  MDNS.begin("tankServer");
+  // Start streaming web server
   startCameraServer();
 }
 
@@ -397,6 +489,6 @@ void loop()
   // Do nothing. Everything is done in another task by the web server
   // delay(1000);
   // Serial.println("{\"direct\":1, \"speed1\":100, \"speed2\":100}");
-  // delay(1000);
+  // delay(100);
   // Serial.println("{\"direct\":0, \"speed1\":200, \"speed2\":200}");
 }
